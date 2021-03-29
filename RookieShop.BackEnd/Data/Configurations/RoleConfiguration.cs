3a34d6966243bfae2a139ff6b4de9c0a293ec335
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-03-29 16:52:14 +0700

    Create DbContext

--- RookieShop.BackEnd/Data/Configurations/RoleConfiguration.cs
+++ RookieShop.BackEnd/Data/Configurations/RoleConfiguration.cs
@@ -1,22 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using RookieShop.BackEnd.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RookieShop.BackEnd.Data.Configurations
 {
-    public class UserConfiguration : IEntityTypeConfiguration<User>
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
     {
-        public void Configure(EntityTypeBuilder<User> builder)
+        public void Configure(EntityTypeBuilder<Role> builder)
         {
             builder.HasKey(o => o.ID);
-
             builder.Property(o => o.ID).UseIdentityColumn();
-
-            builder.Property(o => o.Username).IsRequired();
-
-            builder.Property(o => o.Password).IsRequired();
-
-            builder.Property(o => o.Email).IsRequired();
+            builder.Property(o => o.RoleName).IsRequired();
         }
     }
 }
---
+++ RookieShop.BackEnd/Data/RookieShopDBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RookieShop.BackEnd.Data.Configurations;
+using RookieShop.BackEnd.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieShop.BackEnd.Data
+{
+    public class RookieShopDBContext : DbContext
+    {
+        public RookieShopDBContext(DbContextOptions<RookieShopDBContext> options)
+            : base(options) { }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BrandConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserInRoleConfiguration());
+            modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
